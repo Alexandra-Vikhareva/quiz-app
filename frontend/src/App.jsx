0e063f684df932964cf8f2fb19f327cc212320ca
Sasha <st087955@student.spbu.ru>
2026-07-09 17:45:37 +0300

    Настроена маршрутизация и добавлена заглушка Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 import Login from './components/Login'
+import Dashboard from './components/Dashboard';
+import { AuthProvider } from './context/AuthProvider'
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
 
   return (
-    <Login></Login>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
